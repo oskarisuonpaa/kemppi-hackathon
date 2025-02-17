fde6e39a0114650491b770265bc0c17a256dd4c6
Jomppa69 <joonakivela@hotmail.com>
2025-02-17 14:46:23 +0200

    added user editing

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -2,7 +2,11 @@ import {Router,  Request, Response } from 'express';
 import User from '../models/user';
 import bcrypt from 'bcrypt';
 
-
+const hashedPassword = async (password: string) => {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    return passwordHash
+}
 export const usersRouter = Router();
 
 usersRouter.get('/', async (req: Request, res: Response) => {
@@ -14,13 +18,13 @@ usersRouter.get('/', async (req: Request, res: Response) => {
 usersRouter.post('/', async (req: Request, res: Response) => {
     console.log(req.body)
     const { username, name, password } = req.body
+    console.log(password.length)
 
     if (password.length < 3) {
         res.status(400).json({ error: 'Password must be at least 3 characters long' })
     }
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+    const passwordHash = await hashedPassword(password)
+    
 
     const user = new User({
         username,
@@ -31,4 +35,30 @@ usersRouter.post('/', async (req: Request, res: Response) => {
     const savedUser = await user.save()
 
     res.status(201).json(savedUser)
+});
+
+// Update user
+
+usersRouter.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { username, name, password, role } = req.body;
+
+    const oldUser = await User.findById(id);
+    if (!oldUser) {
+        res.status(404).json({ error: 'User not found' });
+        return;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+        id,
+        { 
+            username: username ?? oldUser.username, 
+            name: name ?? oldUser.name, 
+            password: password ? await hashedPassword(password): oldUser.passwordHash, 
+            role: role ?? oldUser.role 
+        },
+        { new: true, runValidators: true, context: 'query' }
+    );
+
+    res.json(updatedUser);
 });
