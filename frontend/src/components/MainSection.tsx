0ad2b38c8dcea38a0aac335baec62bd5e9f386bf
roitonen <roitonen@outlook.com>
2025-02-17 14:46:02 +0200

    Fonts added

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -11,8 +11,8 @@ const MainSection: React.FC<props> = () => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
-                            <h2 className="hero_title">Hi, it's our team.</h2>
-                            <p className="hero_desc">We are at the Kemppi 2025 hackathon.</p>
+                            <h2 className="hero_title tk-myriad-pro">Hi, it's our team.</h2>
+                            <p className="hero_desc tk-proxima-nova">We are at the Kemppi 2025 hackathon.</p>
                         </div>
                     </div>
                 </div>
