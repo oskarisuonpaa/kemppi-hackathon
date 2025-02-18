62c2d11fbb96f213f25fc8922470f8034a623449
Jomppa69 <joonakivela@hotmail.com>
2025-02-18 13:46:48 +0200

    Some cleaning

--- frontend/src/components/MainSection.tsx
+++ frontend/src/components/MainSection.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-interface props {
-}
-
-const MainSection: React.FC<props> = () => {
+const MainSection: React.FC = () => {
 
     return (
         <>
@@ -11,8 +8,8 @@ const MainSection: React.FC<props> = () => {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
-                            <h2 className="hero_title tk-myriad-pro">Hi, it's our team.</h2>
-                            <p className="hero_desc tk-proxima-nova">We are at the Kemppi 2025 hackathon.</p>
+                            <h2 className="hero_title tk-myriad-pro" >Kemppi data visualiser.</h2>
+                            <p className="hero_desc tk-proxima-nova">Made for Kemppi hackathon 2025, by team spark.</p>
                         </div>
                     </div>
                 </div>
