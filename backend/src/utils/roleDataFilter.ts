dc1d5c42f419d6e4fee003d63c8ffb7a5d11aa55
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-02-18 11:17:24 +0200

    role based filtering actually works now

--- backend/src/utils/roleDataFilter.ts
+++ backend/src/utils/roleDataFilter.ts
@@ -25,12 +25,9 @@ export const roleDataFilter = (role: string, allowedGroups: string[], data: any[
   }
 
   if (role === "viewer") {
-    filteredData = filteredData.map((item) => {
-      const machineGroup = item.weldingMachine?.group;
-      if (allowedGroups.includes(machineGroup)) {
-        return item;
-      }
-    });
+    filteredData = filteredData.filter((item) =>
+      allowedGroups.includes(item.weldingMachine.group)
+    );
   }
 
   return filteredData;
